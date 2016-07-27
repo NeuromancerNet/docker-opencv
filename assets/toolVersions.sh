1be2a9d47bb47b0f810d43713ca6edb9834e25d5
Marco Antonio <marco.silva@bytewood.com>
2016-07-26 22:38:48 -0300

    initial commit

---
+++ assets/toolVersions.sh
@@ -0,0 +1,9 @@
+figlet Neuromancer Tools && echo && \
+java -version && echo  &&\
+python --version && echo && \
+g++ --version && echo && \
+gcc --version && echo && \
+cmake -version && echo && \
+git --version && \
+mvn --version && \
+sleep infinity
